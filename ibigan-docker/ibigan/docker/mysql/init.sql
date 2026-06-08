4802a56aa3d2adfd62ba8002cb63d3d89f7c2d94
Nagibi <nagibi@gmail.com>
2026-06-07 21:06:48 -0300

    chore: remove Zone.Identifier and duplicated files

--- ibigan-docker/ibigan/docker/mysql/init.sql
+++
@@ -1,4 +0,0 @@
--- Necessário para o stancl/tenancy criar automaticamente
--- os bancos ibigan_tenant_* ao cadastrar um novo tenant.
-GRANT CREATE, DROP, ALTER, INDEX, REFERENCES ON *.* TO 'dev'@'%';
-FLUSH PRIVILEGES;
